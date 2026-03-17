026effd5633fc9f6f3c2bf8c8abe29cb2997227f
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2026-03-17 10:33:24 -0300

    Modifique el build

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: "src/index.js",
-      name: "HandedUI",
-      formats: ["es", "cjs"],
-      fileName: (format) => `handed-ui.${format}.js`,
+export default defineConfig(({ mode }) => {
+  if (mode === "lib") {
+    return {
+      plugins: [react()],
+      build: {
+        lib: {
+          entry: "src/index.js",
+          name: "HandedUI",
+          formats: ["es", "cjs"],
+          fileName: (format) => `handed-ui.${format}.js`,
+        },
+        rollupOptions: {
+          external: ["react", "react-dom"],
+        },
+      },
+    };
+  }
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "dist-demo",
     },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-    },
-  },
+  };
 });
